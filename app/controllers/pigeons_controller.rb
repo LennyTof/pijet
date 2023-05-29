6cba0ce3e71c761d81fa870f99d564561d387abc
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2023-05-29 15:53:22 +0200

    add simple form

---
+++ app/controllers/pigeons_controller.rb
@@ -0,0 +1,23 @@
+class PigeonsController < ApplicationController
+
+  def show
+    @pigeon = Pigeon.find(params[:id])
+  end
+
+  def new
+    @pigeon = Pigeon.new
+  end
+
+  def create
+    @pigeon = Pigeon.new(pigeon_params)
+    @pigeon.user = current_user
+    @pigeon.save
+    redirect_to pigeon_path(@pigeon)
+  end
+
+  private
+
+  def pigeon_params
+    params.require(:pigeon).permit(:name, :maximum_payload_weight, :range, :description, :address, :photo)
+  end
+end
