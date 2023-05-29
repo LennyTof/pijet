8514bf6faf0646e4af51e52bb3b7b17f2fab9054
Alexandre Berthon <alexandre_berthon@hotmail.fr>
2023-05-29 15:55:41 +0200

    added index action & view

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,4 +1,7 @@
-class PigeonController < ApplicationController
+class PigeonsController < ApplicationController
+  def index
+    @pigeons = Pigeon.all
+  end
 
   def show
     @pigeon = Pigeon.find(params[:id])
