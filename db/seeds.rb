dc27f1804cf98695801e2c6c84b2d49d2c198088
Alexandre Berthon <alexandre_berthon@hotmail.fr>
2023-05-29 14:58:58 +0200

    Added faker gem and user/pigeon seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Seeding database..."
+Pigeon.destroy_all
+User.destroy_all
+
+3.times do
+  new_user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: "#{Faker::Internet.username(specifier: 5..8)}-#{(rand * 1000).floor}@yopmail.com",
+    password: "123456"
+  )
+  # a user can have 0, 1 or 2 pigeons
+  (rand * 3).floor.times do
+    Pigeon.create!(
+      name: Faker::Name.first_name,
+      description: Faker::Lorem.paragraphs(number: 2).join,
+      maximum_payload_weight: (rand * 100).round(2),
+      range: (rand * 1000).floor,
+      latitude: Faker::Address.latitude,
+      longitude: Faker::Address.longitude,
+      address: Faker::Address.full_address,
+      user: new_user
+    )
+  end
+end
+
+puts "Database seeded with #{User.count} users and #{Pigeon.count} pigeons."
