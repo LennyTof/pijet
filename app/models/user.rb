034144abf7bc91d8a35f6994ab19f85fac3d7e09
Alexandre Berthon <alexandre_berthon@hotmail.fr>
2023-05-30 11:08:51 +0200

    if user is destroyed, related pigeons should be destroyed as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :pigeons
+  has_many :pigeons, dependent: :destroy
   has_many :rentals
   has_many :reviews
 end
