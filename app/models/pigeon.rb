27136a0f70df132865f1cb20e9dd2bc376f5953d
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2023-05-30 17:10:48 +0200

    add radio_buttons to form

--- app/models/pigeon.rb
+++ app/models/pigeon.rb
@@ -3,7 +3,12 @@ class Pigeon < ApplicationRecord
   belongs_to :user
   has_many :rentals
   validates :name, :maximum_payload_weight, :range, :description, :price, presence: true
-
+  WEIGHT = ["10g (ex: 1 USB key)",
+            "20g (ex: 1 SIM CARD)",
+            "30g (ex: 3 USB keys)",
+            "40g (ex: 22 parchment papers)",
+            "50g (ex: 1 SSD Hard drive)",
+            "60g (ex: 1 SSD Hard drive & 1 USB key)"]
   def self.search(search)
     where("name ILIKE ?", "%#{search}%")
   end
