a34cf942dcbb149c16ce291ada4f4c5b27bce290
Antoine Chosson <antoine.chosson@gmail.com>
2023-05-31 14:43:59 +0200

    User dashboard each pigeon infos

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_pigeon, only: %i[show edit update destroy]
 
   def show
     @user = User.find(params[:id])
@@ -10,4 +11,10 @@ class ProfilesController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :photo)
   end
+end
+
+private
+
+def find_pigeon
+  @pigeon = Pigeon.find(params[:id])
 end
