89ca128dd5fed7e758dcb91846baffea759a5d32
alexis nurdin <alexisns@proton.me>
2023-06-01 18:17:29 +0200

    js to button

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :pigeons
   post "pigeons/:id/book", to: "rentals#create", as: :book
   resources :profiles, only: [:show]
-  post "rentals/:id/accept", to: "rentals#accept"
+  post "rentals/:id/accept", to: "rentals#accept", as: :rental_accept
 
   resources :rentals do
     resources :reviews
